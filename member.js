70ca27ccd0bacb4bcd60585df17721072f766521
CyberMastermind <150172557+CyberMastermind@users.noreply.github.com>
2023-11-12 21:21:54 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+ return {
+    restrict: 'E',
+    templateUrl : 'modules/skills/views/members.html',
+    controller: 'SkillsMemberController',
+    controllerAs: 'vm',
+    bindToController: true,
+    scope:{
+        member: 'm'
+    }
+ };
+
+}
